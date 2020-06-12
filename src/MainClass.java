ea20e59e19146223bf3b58b9b3257efe10ce8781
lazekki <lazekkian@gmail.com>
2020-06-12 16:44:07 +0300

    Homework 0.2

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,5 +1,11 @@
 public class MainClass {
 
+    private int class_number = 45;
+
+    public int getClassNumber() {
+        return class_number;
+    }
+
     public int getLocalNumber() {
         int number = 15;
         return number;
--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -4,11 +4,15 @@ public class MainClassTest {
 
     MainClass mainClass = new MainClass();
 
-    public void printTestResult(boolean testResult){
-        if (testResult) {
-            System.out.println("Test passed.");
+    @Test
+    public void testGetClassNumber() {
+        boolean testResult = false;
+        if (mainClass.getClassNumber() > 45) {
+            testResult = true;
+            printTestResult(testResult, "");
         } else {
-            System.out.println("Test failed. Expected number is 14, actual number is " + mainClass.getLocalNumber());
+            testResult = false;
+            printTestResult(testResult, "Expected class number should be more 45, actual class number is " + mainClass.getClassNumber());
         }
     }
 
@@ -17,10 +21,18 @@ public class MainClassTest {
         boolean testResult = false;
         if (mainClass.getLocalNumber() == 14) {
             testResult = true;
+            printTestResult(testResult, "");
         } else {
             testResult = false;
+            printTestResult(testResult, "Expected number is 14, actual number is " + mainClass.getLocalNumber());
         }
-        printTestResult(testResult);
     }
 
+    public void printTestResult(boolean testResult, String message){
+        if (testResult) {
+            System.out.println("Test passed. ");
+        } else {
+            System.out.println("Test failed. " + message);
+        }
+    }
 }
